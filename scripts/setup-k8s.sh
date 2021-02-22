d271854aedf3574d01f02aa5b07dea3dbfb1255f
rooftop90 <ingo.deissenroth@gmail.com>
2021-02-22 22:29:27 +0100

    update k8s tools

---
+++ scripts/setup-k8s.sh
@@ -0,0 +1,18 @@
+################ kubectl ################
+# download kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+# mv kubectl
+mv kubectl /bin
+#########################################
+################# helm ##################
+# version: 3.5.2
+download helm
+wget https://get.helm.sh/helm-v3.5.2-linux-amd64.tar.gz
+
+# untar and move
+tar -xzf helm-v3.5.2-linux-amd64.tar.gz && mv linux-amd64/helm /bin
+
+# cleanup
+rm helm-v3.5.2-linux-amd64.tar.gz && rm -rf linux-amd64
+#########################################
