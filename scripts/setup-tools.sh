3c433ba3d4082c1ee2d03ebc9688187707873bc9
rooftop90 <ingo.deissenroth@gmail.com>
2021-02-25 22:45:24 +0100

    added vault

--- scripts/setup-tools.sh
+++ scripts/setup-tools.sh
@@ -5,3 +5,10 @@ curl https://packages.microsoft.com/config/rhel/7/prod.repo | tee /etc/yum.repos
 # install pwsh
 dnf install powershell -y
 ########################################
+################# vault ################
+# download vault
+wget https://releases.hashicorp.com/vault/1.6.2/vault_1.6.2_linux_amd64.zip
+
+# unzip and move
+unzip vault_1.6.2_linux_amd64.zip && mv vault /bin
+########################################
