e676df288626235c80c9093ea966523e849d0e05
rooftop90 <ingo.deissenroth@gmail.com>
2021-04-27 22:03:04 +0200

    feat(tools): :wrench:  added gacp

--- scripts/setup-tools.sh
+++ scripts/setup-tools.sh
@@ -12,3 +12,10 @@ wget https://releases.hashicorp.com/vault/1.6.2/vault_1.6.2_linux_amd64.zip
 # unzip and move
 unzip vault_1.6.2_linux_amd64.zip && mv vault /bin
 ########################################
+################# gacp #################
+# install nodejs
+dnf install nodejs -y
+
+# install gacp
+npm i -g gacp
+########################################
